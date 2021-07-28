20b063de0143fdf2e2d916bafb9eaca1baa55b87
MOHAMMAD IQBAL <MOHAMMAD IQBAL@DESKTOP-4F147PC.mshome.net>
2021-07-28 21:05:28 +0530

    commit us-2

--- TestGitProj/src/com/test/MyClass.java
+++ TestGitProj/src/com/test/MyClass.java
@@ -3,7 +3,7 @@ package com.test;
 public class MyClass {
 
 	void add() {
-		System.out.println("hello method");
+		System.out.println("hello method jkjk");
 	}
 	
 	public static void main(String[] args) {
